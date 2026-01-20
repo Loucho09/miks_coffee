dd40a1caeb8cd2b505d853958cec1bcafd65773c
Loucho09 <jmloucho09@gmail.com>
2026-01-20 21:10:31 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- config.js -->
     <script>
     tailwind.config = {
         darkMode: 'class',
