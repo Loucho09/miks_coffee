bd13a6beadf22704f911ec2909337ae4ffb79d23
Loucho09 <jmloucho09@gmail.com>
2026-03-02 23:12:53 +0800

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -59,7 +59,8 @@
                                 $deviceModel = 'iPhone' . (isset($matches[1]) ? ' (iOS ' . str_replace('_', '.', $matches[1]) . ')' : '');
                             } elseif (str_contains($lowerAgent, 'android')) {
                                 preg_match('/Android \d+; ([^;)]+)/', $agent, $matches);
-                                $deviceModel = isset($matches[1]) ? trim($matches[1]) : 'Android Device';
+                                $foundModel = isset($matches[1]) ? trim($matches[1]) : 'Android Device';
+                                $deviceModel = ($foundModel === 'K') ? 'Android Mobile' : $foundModel;
                             } elseif (str_contains($lowerAgent, 'ipad')) {
                                 $deviceModel = 'iPad';
                             } elseif (str_contains($lowerAgent, 'windows')) {
