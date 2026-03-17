26d62a7fae86b4486ba22de72e2e1306ad86d660
Loucho09 <jmloucho09@gmail.com>
2026-03-17 17:25:41 +0800

    Update public_menu.blade.php

--- resources/views/public_menu.blade.php
+++ resources/views/public_menu.blade.php
@@ -200,5 +200,5 @@
             if (localStorage.getItem('color-theme')) { if (localStorage.getItem('color-theme') === 'light') { document.documentElement.classList.add('dark'); localStorage.setItem('color-theme', 'dark'); } else { document.documentElement.classList.remove('dark'); localStorage.setItem('color-theme', 'light'); } } else { if (document.documentElement.classList.contains('dark')) { document.documentElement.classList.remove('dark'); localStorage.setItem('color-theme', 'light'); } else { document.documentElement.classList.add('dark'); localStorage.setItem('color-theme', 'dark'); } }
         });  
     </script>
-</body>
+</body> 
 </html> 
