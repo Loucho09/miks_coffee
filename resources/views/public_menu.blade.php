7a34def2174970791f241f697c468c1ca560ff75
Loucho09 <jmloucho09@gmail.com>
2026-03-18 12:10:20 +0800

    Update public_menu.blade.php

--- resources/views/public_menu.blade.php
+++ resources/views/public_menu.blade.php
@@ -199,6 +199,6 @@
             darkIcon.classList.toggle('hidden'); lightIcon.classList.toggle('hidden');
             if (localStorage.getItem('color-theme')) { if (localStorage.getItem('color-theme') === 'light') { document.documentElement.classList.add('dark'); localStorage.setItem('color-theme', 'dark'); } else { document.documentElement.classList.remove('dark'); localStorage.setItem('color-theme', 'light'); } } else { if (document.documentElement.classList.contains('dark')) { document.documentElement.classList.remove('dark'); localStorage.setItem('color-theme', 'light'); } else { document.documentElement.classList.add('dark'); localStorage.setItem('color-theme', 'dark'); } }
         });  
-    </script> 
+    </script>  
 </body>
 </html>
