e68a4200c426f39ae0ac2a5a60b642c71c01e1f5
Loucho09 <jmloucho09@gmail.com>
2026-04-03 00:07:32 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,7 @@ class HomeController extends Controller
         $isHappyHour = Product::where('is_active', 1)
             ->get()
              ->some(fn($p) => $p->is_happy_hour_active);
-
+ 
         return view('welcome', compact('topStreaks', 'featuredProducts', 'trendingProducts', 'isHappyHour'));
     }
 }  
