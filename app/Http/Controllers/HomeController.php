c60f5aafd12253816673accb686b15dd606fa6a5
Loucho09 <jmloucho09@gmail.com>
2026-04-03 00:07:37 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
                  ->take(3)
                    ->get();
 
-        // 🟢 FEATURE: Check if any active Happy Hour exists
+         // 🟢 FEATURE: Check if any active Happy Hour exists
         $isHappyHour = Product::where('is_active', 1)
             ->get()
              ->some(fn($p) => $p->is_happy_hour_active);
